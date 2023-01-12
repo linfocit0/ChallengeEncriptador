cbf9c163ae3b888c669c0d427a4efe30a6b8bb8b
RamonSixto <ramonsixtoportillo@gmail.com>
2023-01-12 18:05:53 -0300

    terminado... por ahora

--- index.js
+++ index.js
@@ -18,24 +18,26 @@ function validacionYEncriptacion(){
 //FUNCION AMBIENTE DE ALERTA Y NORMAL
 function ambienteDeAlerta(){
 	document.getElementById("textoDisclaimer").style.color = "red";
-        document.getElementById("textoDisclaimer").style.fontWeight = "bolder";
-        document.getElementById("textoDisclaimer").style.fontSize = "30px";
-        verInput.style.borderColor="red";
-        verInput.style.color="white";
-        verInput.style.borderWidth="thick";
-        verInput.style.backgroundColor="red";
-        document.getElementById("hacerDesaparecer").style.display="flex";
-        document.getElementById("campoOculto").style.display="none";
+    document.getElementById("textoDisclaimer").style.fontWeight = "bolder";
+    document.getElementById("textoDisclaimer").style.fontSize = "30px";
+    verInput.style.borderColor="red";
+    verInput.style.color="white";
+    verInput.style.borderWidth="thick";
+    verInput.style.backgroundColor="red";
+    document.getElementById("textoDisclaimer2").style.display="inline";
+    document.getElementById("hacerDesaparecer").style.display="flex";
+    document.getElementById("campoOculto").style.display="none";
 }
 
 function ambienteNormal(){
     document.getElementById("textoDisclaimer").style.color = "#000000";
-        document.getElementById("textoDisclaimer").style.fontWeight = "auto";
-        document.getElementById("textoDisclaimer").style.fontSize = "16px";
-        verInput.style.borderColor="#767676";
-        verInput.style.color="#000000";
-        verInput.style.borderWidth="1px";
-        verInput.style.backgroundColor="#FFFFFF";
+    document.getElementById("textoDisclaimer").style.fontWeight = "auto";
+    document.getElementById("textoDisclaimer").style.fontSize = "16px";
+    verInput.style.borderColor="#767676";
+    verInput.style.color="#000000";
+    verInput.style.borderWidth="1px";
+    verInput.style.backgroundColor="#FFFFFF";
+    document.getElementById("textoDisclaimer2").style.display="none";
 }
 botonEncriptar.addEventListener("click", validacionYEncriptacion);
 
